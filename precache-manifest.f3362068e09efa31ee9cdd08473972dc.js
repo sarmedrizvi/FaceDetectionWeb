8b327fa9c58ff726fe5a0b49edfb7579f769d60f
sarmedrizvi <sarmedrizvi@gmail.com>
2019-12-26 12:41:51 +0500

    Updates

---
+++ precache-manifest.f3362068e09efa31ee9cdd08473972dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b92d3e6b4a1dbc0e26bdb897eadf3f",
+    "url": "sarmedrizvi.github.io/FaceDetectionWeb/index.html"
+  },
+  {
+    "revision": "1c2cb0d7c8bcdfc635c5",
+    "url": "sarmedrizvi.github.io/FaceDetectionWeb/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1c85a0d8ec6b9b42d7e1",
+    "url": "sarmedrizvi.github.io/FaceDetectionWeb/static/css/main.86e548a5.chunk.css"
+  },
+  {
+    "revision": "1c2cb0d7c8bcdfc635c5",
+    "url": "sarmedrizvi.github.io/FaceDetectionWeb/static/js/2.f9066644.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "sarmedrizvi.github.io/FaceDetectionWeb/static/js/2.f9066644.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c85a0d8ec6b9b42d7e1",
+    "url": "sarmedrizvi.github.io/FaceDetectionWeb/static/js/main.6e1132c8.chunk.js"
+  },
+  {
+    "revision": "01d34b5c11977d732c92",
+    "url": "sarmedrizvi.github.io/FaceDetectionWeb/static/js/runtime-main.576e71dd.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "sarmedrizvi.github.io/FaceDetectionWeb/precache-manifest.f3362068e09efa31ee9cdd08473972dc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("sarmedrizvi.github.io/FaceDetectionWeb/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
