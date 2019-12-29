093f857847896db8ce787f27e14912b106c39212
sarmedrizvi <sarmedrizvi@gmail.com>
2019-12-29 20:53:09 +0500

    Updates

---
+++ precache-manifest.d989c22587b1b1541d3a710211148965.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b02e163de25da042005fdedac389b1",
+    "url": "/FaceDetectionWeb/index.html"
+  },
+  {
+    "revision": "7b0989059b1b2f5fb10d",
+    "url": "/FaceDetectionWeb/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1a1d360c1822886eee17",
+    "url": "/FaceDetectionWeb/static/css/main.da9ce433.chunk.css"
+  },
+  {
+    "revision": "7b0989059b1b2f5fb10d",
+    "url": "/FaceDetectionWeb/static/js/2.ce459f8d.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/FaceDetectionWeb/static/js/2.ce459f8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a1d360c1822886eee17",
+    "url": "/FaceDetectionWeb/static/js/main.cac45fd9.chunk.js"
+  },
+  {
+    "revision": "9beea41791be6378d153",
+    "url": "/FaceDetectionWeb/static/js/runtime-main.af85b8bb.js"
+  }
+]);
